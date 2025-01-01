87950613db3e23caffa47cf6a289f71e0f35748c
Skyler Carlson <skylercarlson28@gmail.com>
2025-01-01 14:37:34 -0800

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+log_file="deploy.log"
+exec > >(tee -i $log_file)
+exec 2>&1
+
+echo "Pulling from git"
+if ! git pull git@github.com:skycarl/iou_app.git; then
+  echo "Failed to pull from git"
+  exit 1
+fi
+
+echo "Pulling Docker images"
+if ! docker pull ghcr.io/skycarl/iou_app:latest; then
+  echo "Failed to pull iou_app image"
+  exit 1
+fi
+
+if ! docker pull ghcr.io/skycarl/iou_bot:latest; then
+  echo "Failed to pull iou_bot image"
+  exit 1
+fi
+
+echo "Restarting Docker containers"
+if ! docker compose -f docker-compose.yml down; then
+  echo "Failed to stop Docker containers"
+  exit 1
+fi
+
+if ! docker compose -f docker-compose.yml up -d; then
+  echo "Failed to start Docker containers"
+  exit 1
+fi
+
+echo "Deployment completed successfully"
