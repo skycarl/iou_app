87ebd7996a51432ab37ff31bd15ff290e73bef46
Skyler Carlson <skylercarlson28@gmail.com>
2025-06-29 17:37:15 -0700

    Handle empty queries

--- iou_app/iou/utils.py
+++ iou_app/iou/utils.py
@@ -26,13 +26,30 @@ def compute_iou_status(query1: List[EntrySchema], query2: List[EntrySchema]):
     user1_sum = sum([entry.amount for entry in query1])
     user2_sum = sum([entry.amount for entry in query2])
 
+    # Handle empty queries
+    if not query1 and not query2:
+        err_msg = f'IOU status not found with query1={query1} and query2={query2}'
+        logger.error(err_msg)
+        raise ValueError(err_msg)
+
+    # Get user names from available queries
+    if query1:
+        user1_name = query1[0].sender
+        user2_name = query1[0].recipient
+    else:
+        user1_name = query2[0].recipient  # Reverse since this is user2's query
+        user2_name = query2[0].sender
+
     # Compute the IOU status
     if user1_sum > user2_sum:
-        iou_status = {'user1': query1[0].sender, 'user2': query1[0].recipient, 'amount': user1_sum - user2_sum}
+        iou_status = {'user1': user1_name, 'user2': user2_name, 'amount': user1_sum - user2_sum}
     elif user2_sum > user1_sum:
-        iou_status = {'user1': query2[0].sender, 'user2': query2[0].recipient, 'amount': user2_sum - user1_sum}
+        # When user2 owes more, the owing user should be from query2 if available
+        owing_user = query2[0].sender if query2 else user2_name
+        owed_user = query2[0].recipient if query2 else user1_name
+        iou_status = {'user1': owing_user, 'user2': owed_user, 'amount': user2_sum - user1_sum}
     elif user1_sum == user2_sum:
-        iou_status = {'user1': query1[0].sender, 'user2': query1[0].recipient, 'amount': 0.}
+        iou_status = {'user1': user1_name, 'user2': user2_name, 'amount': 0.}
     else:
         err_msg = f'IOU status not found with query1={query1} and query2={query2}'
         logger.error(err_msg)
